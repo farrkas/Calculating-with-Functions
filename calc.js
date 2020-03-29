b163fda43cb6e6ae3043eb38ac6302f63742d92e
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-03-29 15:21:32 +0200

    przeszło testy

--- calc.js
+++ calc.js
@@ -1,32 +1,4 @@
 // https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/javascript
-
-function zero(z) {
-
-    const x = {
-        value: 0,
-        action: "",
-    }
-    //if (z.value===null || z.value===undefined) {}
-    const y = { value, action } = z;
-    let result = { action: y.action }
-    /*
-        switch (result.action) {
-            case "add": result.value = x.value + y.value; break;
-            case "minus": result.value = x.value - y.value; break;
-            case "times": result.value = x.value * y.value; break;
-            case "devidedBy":
-                if (y.value == 0) { console.log("dzielenie przez 0!!!!!!") }
-                else { result.value = x.value + y.value; break; }
-            default: break;
-        }
-    
-    
-    */
-    //  return result; 
-    if (result.action === null || result.action === undefined) console.log(result);
-    else console.log(result.value);
-}
-
 function variable(z, x) {
     if (z != undefined) {
         const y = { value, action } = z;
@@ -38,144 +10,116 @@ function variable(z, x) {
             case "times": result.value = x.value * y.value; break;
             case "devidedBy":
                 if (y.value == 0) { console.log("dzielenie przez 0!!!!!!") }
-                else { result.value = x.value + y.value; break; }
+                else { result.value = Math.floor(x.value / y.value); break; }
             default: break;
         }
-
-        //  return result; 
-        console.log("one "); console.log(result.value);
+        //console.log("result");
+        console.log(result.value);
+        return result.value;
     }
-    else { console.log(x); return x; }
+    else {
+        //console.log("varriable"); console.log(x);
+        return x;
+    }
 }
-function one(z) {
+
+function zero(q) {
+    const x = {
+        value: 0,
+        action: "",
+    }
+    const next = variable(q, x);
+    //console.log("zero");
+    //console.log(next);
+    return next;
+}
+function one(q) {
     const x = {
         value: 1,
     }
-    variable(z, x);
+    const next = variable(q, x);
+    //console.log("one");
+    //console.log(next);
+    return next;
 }
-function two(z) {
+function two(q) {
     const x = {
         value: 2,
         action: "",
     }
-
-    if (z != undefined) {
-        const y = { value, action } = z;
-        let result = { action: y.action, }
-
-        switch (result.action) {
-            case "add": result.value = x.value + y.value; break;
-            case "minus": result.value = x.value - y.value; break;
-            case "times": result.value = x.value * y.value; break;
-            case "devidedBy":
-                if (y.value == 0) { console.log("dzielenie przez 0!!!!!!") }
-                else { result.value = x.value + y.value; break; }
-            default: break;
-        }
-
-        //  return result; 
-        console.log("two "); console.log(result.value);
-    }
-    else { return x; }
+    const next = variable(q, x);
+    //console.log("two");
+    //console.log(next);
+    return next;
 }
-function three(z) {
+function three(q) {
     const x = {
         value: 3,
         action: "",
     }
-
-    if (z != undefined) {
-        const y = { value, action } = z;
-        let result = { action: y.action, }
-
-        switch (result.action) {
-            case "add": result.value = x.value + y.value; break;
-            case "minus": result.value = x.value - y.value; break;
-            case "times": result.value = x.value * y.value; break;
-            case "devidedBy":
-                if (y.value == 0) { console.log("dzielenie przez 0!!!!!!") }
-                else { result.value = x.value + y.value; break; }
-            default: break;
-        }
-
-        //  return result; 
-        console.log("two "); console.log(result.value);
-    }
-    else { return x; }
+    const next = variable(q, x);
+    //console.log("three");
+    //console.log(next);
+    return next;
 }
-function four(z) {
+function four(q) {
     const x = {
         value: 4,
         action: "",
     }
-
-    if (z != undefined) {
-        const y = { value, action } = z;
-        let result = { action: y.action, }
-
-        switch (result.action) {
-            case "add": result.value = x.value + y.value; break;
-            case "minus": result.value = x.value - y.value; break;
-            case "times": result.value = x.value * y.value; break;
-            case "devidedBy":
-                if (y.value == 0) { console.log("dzielenie przez 0!!!!!!") }
-                else { result.value = x.value + y.value; break; }
-            default: break;
-        }
-
-        //  return result; 
-        console.log("two "); console.log(result.value);
-    }
-    else { return x; }
+    const next = variable(q, x);
+    //console.log("four");
+    //console.log(next);
+    return next;
 }
-function five(z) {
+function five(q) {
     const x = {
         value: 5,
         action: "",
     }
-
-    if (z != undefined) {
-        const y = { value, action } = z;
-        let result = { action: y.action, }
-
-        switch (result.action) {
-            case "add": result.value = x.value + y.value; break;
-            case "minus": result.value = x.value - y.value; break;
-            case "times": result.value = x.value * y.value; break;
-            case "devidedBy":
-                if (y.value == 0) { console.log("dzielenie przez 0!!!!!!") }
-                else { result.value = x.value + y.value; break; }
-            default: break;
-        }
-
-        //  return result; 
-        console.log("two "); console.log(result.value);
-    }
-    else { return x; }
+    const next = variable(q, x);
+    //console.log("five");
+    //console.log(next);
+    return next;
 }
-function six(z) {
+function six(q) {
     const x = {
         value: 6,
         action: "",
     }
+    const next = variable(q, x);
+    //console.log("six");
+    //console.log(next);
+    return next;
 }
-function seven(z) {
+function seven(q) {
     const x = {
         value: 7,
-
     }
+    const next = variable(q, x);
+    //console.log("seven");
+    //console.log(next);
+    return next;
 }
-function eight(z) {
+function eight(q) {
     const x = {
         value: 8,
         action: "",
     }
+    const next = variable(q, x);
+    //console.log("eight");
+    //console.log(next);
+    return next;
 }
-function nine(z) {
+function nine(q) {
     const x = {
         value: 9,
         action: "",
     }
+    const next = variable(q, x);
+    //console.log("nine");
+    //console.log(next);
+    return next;
 }
 
 function plus(z) {
@@ -183,7 +127,7 @@ function plus(z) {
         value: z.value,
         action: "add",
     }
-    console.log("plus "); console.log(x);
+    //  console.log("plus "); console.log(x);
     return x;
 }
 function minus(z) {
@@ -191,7 +135,7 @@ function minus(z) {
         value: z.value,
         action: "minus",
     }
-    console.log("minus "); console.log(x);
+    //console.log("minus "); console.log(x);
     return x;
 }
 function times(z) {
@@ -199,7 +143,7 @@ function times(z) {
         value: z.value,
         action: "times",
     }
-    console.log("times "); console.log(x);
+    //console.log("times "); console.log(x);
     return x;
 }
 function dividedBy(z) {
@@ -207,12 +151,12 @@ function dividedBy(z) {
         value: z.value,
         action: "devidedBy",
     }
-    console.log("devidedBy "); console.log(x);
+    //console.log("devidedBy "); console.log(x);
     return x;
 }
 
-one(plus(one()));
-//seven(times(five()))  // 35;
-//four(plus(nine()))    // 13;
-//eight(minus(three())) // 5;
-//six(dividedBy(two())) // 3;
+
+seven(times(five())); console.log("oczekiwane 35");
+four(plus(nine())); console.log("oczekiwane 13");
+eight(minus(three())); console.log("oczekiwane 5");
+six(dividedBy(two())); console.log("oczekiwane 3");
